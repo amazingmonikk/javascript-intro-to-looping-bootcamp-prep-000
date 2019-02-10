3aa5ed14b8863bf7ca0610bc13d8c84b08315d01
Monika Tadross <monikk.tadross@gmail.com>
2019-02-10 23:40:14 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,34 @@
+function forLoop(array) {
+
+for (let i = 0; i < 25; i++)
+{
+if (i === 1) {
+  console.log(array.push(`I am 1 strange loop.`));
+} else {
+  console.log(array.push( `I am ${i} strange loops.`));
+}
+}
+return array;
+}
+
+
+function whileLoop(number) {
+  while(number > 0){
+    console.log(--number);
+  }
+    return `done`
+  
+}
+
+
+
+function doWhileLoop(integer) {
+
+do { 
+   var i = 0;
+   i = i + 1;
+   console.log("I run once regardless.");
+} while
+( i < integer);
+}
+  
